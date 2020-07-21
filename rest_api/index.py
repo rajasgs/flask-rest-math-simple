84fe3abd8f7a0d079da3c6e846a657b6f17be080
Raja Raman <raja.raman@kwikee.com>
2020-07-21 15:35:04 -0400

    Updates for 2020-07-21

--- rest_api/index.py
+++ rest_api/index.py
@@ -1,5 +1,13 @@
 from . import api
 
+import time
+import random
+
 @api.route('/')
 def index():
-    return "inside index"
+
+    time.sleep(5)
+
+    r_number = random.randint(0, 1000)
+
+    return "inside index : " +str(r_number)
