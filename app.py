b26c820301bb49fd213a7512777f86dfc7562fb2
Raja CSP <raja.raman@syndigo.com>
2022-05-05 19:20:00 -0400

    Info socket updated

--- app.py
+++ app.py
@@ -4,13 +4,28 @@
         
 '''
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 from rest_api import *
 import os
 
 app = Flask(__name__)
 
 app.register_blueprint(api)
+
+socketio = SocketIO(app)
+
+@app.route("/tinfo/admin", methods = ['GET', 'POST'])
+def admin():
+    return render_template("index.html")
+
+@app.route("/tinfo", methods = ['GET', 'POST'])
+def index():
+    return render_template("message.html")
+
+@socketio.on('connect')
+def connected():
+    print('connect')
 
 '''
 @app.route("/")
@@ -19,6 +34,8 @@ def hello():
 '''
 
 if __name__ == "__main__":
+
+    socketio.run(app, debug=True)
     
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
